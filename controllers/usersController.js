f57b4292cd6268e16a89c5cceaf6b9330b1886b6
ArtyomHloba <artemgloba2@gmail.com>
2024-10-05 17:34:06 +0300

    Add CRUD

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,6 +3,7 @@ const { User } = require('./../models');
 
 module.exports.createUser = async (req, res, next) => {
   const { body } = req;
+
   try {
     const createdUser = await User.create(body);
     res.status(201).send(createdUser);
@@ -13,26 +14,59 @@ module.exports.createUser = async (req, res, next) => {
 };
 
 module.exports.getAllUsers = async (req, res, next) => {
-  res.status(501).send('Not Implemented');
+  const { page, results } = req.query;
+
+  const limit = results;
+  const offset = (page - 1) * results;
+
+  try {
+    const foundUsers = await User.getAll(limit, offset);
+
+    res.status(200).send(foundUsers);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports.getUserById = async (req, res, next) => {
   const { userId } = req.params;
+
   try {
     const foundUser = await User.getById(userId);
+
     if (!foundUser) {
+      // return res.status(404).send('User Not Found');
       return next(createHttpError(404, 'User Not Found'));
     }
+
     res.status(200).send(foundUser);
   } catch (err) {
     next(err);
   }
 };
 
-module.exports.updateUserById = async (req, res, next) => {};
+module.exports.updateUserById = async (req, res, next) => {
+  const {
+    params: { userId },
+    body,
+  } = req;
+
+  try {
+    const updatedUser = await User.updateById(body, userId);
+
+    if (!updatedUser) {
+      return next(createHttpError(404, 'User Not Found'));
+    }
+
+    res.status(200).send(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.deleteUserById = async (req, res, next) => {
   const { userId } = req.params;
+
   try {
     const foundUser = await User.deleteById(userId);
     if (!foundUser) {
--- models/user.js
+++ models/user.js
@@ -1,18 +1,33 @@
 class User {
   static async create ({ firstName, lastName, email, tel }) {
     try {
+      const query = `
+        INSERT INTO users (first_name, last_name, email, tel)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *; 
+      `;
+
       const {
         rows: [createdUser],
-      } = await User.pool.query(
-        `
-          INSERT INTO users (first_name, last_name, email, tel)
-          VALUES ($1, $2, $3, $4)
-          RETURNING *; 
-        `,
-        [firstName, lastName, email, tel]
-      );
+      } = await User.pool.query(query, [firstName, lastName, email, tel]);
 
       return createdUser;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  static async getAll (limit, offset) {
+    try {
+      const query = `
+        SELECT *
+        FROM users
+        ORDER BY id
+        LIMIT $1 OFFSET $2
+      `;
+      const { rows } = await User.pool.query(query, [limit, offset]);
+
+      return rows;
     } catch (err) {
       throw err;
     }
@@ -22,9 +37,10 @@ class User {
     try {
       const query = `
       SELECT *
-      FROM users
+      FROM users 
       WHERE id = $1;
     `;
+
       const {
         rows: [foundUser],
       } = await User.pool.query(query, [userId]);
@@ -34,6 +50,35 @@ class User {
       throw err;
     }
   }
+
+  static async updateById ({ firstName, lastName, email, tel }, userId) {
+    try {
+      const query = `
+        UPDATE users
+        SET first_name = $1, 
+            last_name = $2,
+            email = $3,
+            tel = $4
+        WHERE id = $5
+        RETURNING *;    
+      `;
+
+      const {
+        rows: [updatedUser],
+      } = await User.pool.query(query, [
+        firstName,
+        lastName,
+        email,
+        tel,
+        userId,
+      ]);
+
+      return updatedUser;
+    } catch (err) {
+      throw err;
+    }
+  }
+
   static async deleteById (userId) {
     try {
       const query = `
